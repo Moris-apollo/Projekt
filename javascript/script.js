248f92d4bba17f87889b7824eb98830b88c19bd9
Moris <77972196+Moris-apollo@users.noreply.github.com>
2022-11-07 19:48:18 +0100

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,27 @@
 function showErrors() {
     if (document.getElementById("imie").value=="") {
         alert("Uzupełnij pole \"imie\"!");
+    } else {
+        document.imie = document.getElementById("imie").value;
     }
 
     if (document.getElementById("nazwisko").value=="") {
         alert("Uzupełnij pole \"nazwisko\"!");
-    }
-    
-    if (document.getElementById("plec").value=="") {
-        alert("Zaznacz odpowiednią pozycje w polu \"płeć\"!");
+    } else {
+        document.nazwisko = document.getElementById("nazwisko").value;
     }
 
-    if (document.getElementById("kolor").value=="") {
-        alert("Wybierz kolor");
+    if (document.getElementById("plec").value.toString=="") {
+        alert("Zaznacz odpowiednią pozycje w polu \"płeć\"!");
+    } else {
+        document.plec = document.getElementById("plec").value;
     }
+
+    if (document.getElementById("kolor").value.toString=="") {
+        alert("Wybierz kolor");
+    } else {
+        document.kolor = document.getElementById("kolor").value;
+    }
+
+    document.data_ur = document.getElementById("data_ur").value.toString
 }
